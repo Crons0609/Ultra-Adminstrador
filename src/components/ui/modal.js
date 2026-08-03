3e5463f9ba9a54d1db84d6c87d81711d06c30819
Crons0609 <christhiam.d.r.v.1590@gmail.com>
2026-08-02 22:36:28 -0600

    0.9.8

--- src/components/ui/modal.js
+++ src/components/ui/modal.js
@@ -62,6 +62,32 @@ export class Modal extends Component {
           this.close();
         }
       });
+
+      // Prevent pull-to-refresh gesture when dragging down at top of modal
+      const modalBody = this.$('.modal-body');
+      let startY = 0;
+
+      this._touchstartHandler = (e) => {
+        if (e.touches && e.touches.length === 1) {
+          startY = e.touches[0].clientY;
+        }
+      };
+
+      this._touchmoveHandler = (e) => {
+        if (!e.touches || e.touches.length !== 1) return;
+        const currentY = e.touches[0].clientY;
+        const deltaY = currentY - startY;
+
+        if (modalBody) {
+          // If swiping down at top of modal scroll, prevent pull-to-refresh
+          if (modalBody.scrollTop <= 0 && deltaY > 0) {
+            e.preventDefault();
+          }
+        }
+      };
+
+      this.element.addEventListener('touchstart', this._touchstartHandler, { passive: true });
+      this.element.addEventListener('touchmove', this._touchmoveHandler, { passive: false });
     }
   }
 
@@ -73,6 +99,11 @@ export class Modal extends Component {
   }
 
   unmount() {
+    if (this.element) {
+      if (this._touchstartHandler) this.element.removeEventListener('touchstart', this._touchstartHandler);
+      if (this._touchmoveHandler) this.element.removeEventListener('touchmove', this._touchmoveHandler);
+    }
+
     // Remove this modal's element from DOM first
     if (this.element && this.element.parentNode) {
       this.element.parentNode.removeChild(this.element);
