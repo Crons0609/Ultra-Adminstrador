fbaeaf1ffa3258cfa47b7359c49906a7dcb9e688
Crons0609 <christhiam.d.r.v.1590@gmail.com>
2026-08-09 20:13:59 -0600

    1.3.9

--- android/app/src/main/assets/src/config/app.config.js
+++ android/app/src/main/assets/src/config/app.config.js
@@ -20,7 +20,7 @@ export const APP_CONFIG = {
   copyright: '© 2026 Ultra Administrador by ProLine System. Todos los derechos reservados.',
 
   /** Application version — bump on each release */
-  version: '1.0.0',
+  version: '1.3.8',
 
   /** Default theme class on HTML body. Options: 'theme-dark' | 'theme-light' */
   defaultTheme: 'theme-dark',
